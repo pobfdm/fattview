68560236cae079f78d14ca2ecb5be09cf9e80e1f
Fabio Di Matteo <pobfdm@gmail.com>
2019-10-13 15:55:33 +0200

    Added support to p7m files

--- efatt.py
+++ efatt.py
@@ -1,61 +1,61 @@
 import xml.etree.ElementTree as ET
 
 class fatt():
-	
+
 	def __init__(self, xmlfile):
 		self.tree = ET.parse(xmlfile)
-		
-	
+
+
 	def getTextFromNode(self,node,e=0):
 		''' Specificare il path del nodo (esempio: "./FatturaElettronicaHeader/DatiTrasmissione/IdTrasmittente/IdPaese") '''
 		root = self.tree.getroot()
 		try:
 			return(root.findall(node)[e].text)
 		except IndexError:
-			return "VUOTO"
-	
-	
-	
+			return""
+
+
+
 	def getCountLinee(self):
 		root = self.tree.getroot()
 		r=0
 		try:
 			for c in root.findall('./FatturaElettronicaBody/DatiBeniServizi/DettaglioLinee/NumeroLinea'):
 				r=r+1
-			return r	
-		except IndexError:			
 			return r
-		
+		except IndexError:
+			return r
+
 	def getDescrizioneFromLinea(self,l):
 		root = self.tree.getroot()
 		try:
 			return(root.findall("./FatturaElettronicaBody/DatiBeniServizi/DettaglioLinee/Descrizione")[l-1].text)
 		except IndexError:
-			return "VUOTO"
-			
+			return""
+
 	def printAllDescrizioni(self): #for testing
 		root = self.tree.getroot()
 		n=0
-		for d in root.findall('./FatturaElettronicaBody/DatiBeniServizi/DettaglioLinee/Descrizione'):		
+		for d in root.findall('./FatturaElettronicaBody/DatiBeniServizi/DettaglioLinee/Descrizione'):
 			n=n+1
 			print("Descrizione numero: %d\n %s" %  (n, d.text))
-	
+
 	def printAllRiferimentoNumeroLinea(self): #for testing
 		root = self.tree.getroot()
 		n=0
 		res=[]
-		for d in root.findall('./FatturaElettronicaBody/DatiGenerali/DatiOrdineAcquisto/RiferimentoNumeroLinea'):		
+		for d in root.findall('./FatturaElettronicaBody/DatiGenerali/DatiOrdineAcquisto/RiferimentoNumeroLinea'):
 			n=n+1
 			print("RiferimentoNumeroLinea:  %s" %  (d.text))
 			res.append(d.text)
-	
-	
+
+
 	def countChildElementByName(self,node):
 		root = self.tree.getroot()
 		r=0
 		try:
 			for c in root.findall(node):
 				r=r+1
-			return r	
-		except IndexError:			
+			return r
+		except IndexError:
 			return r
--- utils.py
+++ utils.py
@@ -1,5 +1,51 @@
 import os
+import re
+from gzip import GzipFile
+
+
 
 def getTempDir():
 	import tempfile
 	return tempfile.gettempdir()+os.sep
+
+def getFileExt(filename):
+	extension = os.path.splitext(filename)[1][1:]
+	return extension
+
+def getXMLfromP7m(filename):
+	from OpenSSL import crypto
+	from OpenSSL._util import (
+	    ffi as _ffi,
+	    lib as _lib,
+	)
+
+	# Or, alternatively:
+	# from cryptography.hazmat.bindings.openssl.binding import Binding
+	# _lib = Binding.lib
+	# _ffi = Binding.ffi
+
+	with open(filename, 'rb') as f:
+	    p7data = f.read()
+	#p7 = crypto.load_pkcs7_data(crypto.FILETYPE_ASN1, p7data)
+	p7 = crypto.load_pkcs7_data(crypto.FILETYPE_ASN1, p7data)
+
+	bio_out =crypto._new_mem_buf()
+	res = _lib.PKCS7_verify(p7._pkcs7, _ffi.NULL, _ffi.NULL, _ffi.NULL, bio_out, _lib.PKCS7_NOVERIFY)
+	if res == 1:
+		databytes = crypto._bio_to_string(bio_out)
+		#databytes = databytes.decode('utf8')
+		#databytes = databytes.replace('\ufeff', "")
+		#print(databytes)
+
+	else:
+	    errno = _lib.ERR_get_error()
+	    errstrlib = _ffi.string(_lib.ERR_lib_error_string(errno))
+	    errstrfunc = _ffi.string(_lib.ERR_func_error_string(errno))
+	    errstrreason = _ffi.string(_lib.ERR_reason_error_string(errno))
+
+
+	xmlFileName=getTempDir()+'tmpFatt.xml'
+	open(xmlFileName, 'wb').write(databytes)
+
+
+	return xmlFileName
